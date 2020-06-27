eef284112726ed47c1eba59bf3400243dab35a9f
Erik Lind <erik@Administrators-Mac-mini.local>
2020-06-27 14:39:40 -0500

    code review

--- lesson_2/rock_paper_scissors.js
+++ lesson_2/rock_paper_scissors.js
@@ -1,8 +1,22 @@
+// capitalize function?
+
+
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard'];
+const MESSAGES = require('./rock_paper_scissors_messages.json');
+
+const VALID_GAME_CHOICE_INPUT = {
+  rock     : ['rock'    , 'r' ],
+  paper    : ['paper'   , 'p' ],
+  scissors : ['scissors', 'sc'],
+  spock    : ['spock'   , 'sp'],
+  lizard   : ['lizard'  , 'l' ],
+};
+
+const VALID_CHOICES = Object.keys(VALID_GAME_CHOICE_INPUT);
+
 const VALID_PLAY_AGAIN_CHOICES = {
   yes : ['yes', 'y'],
-  no : ['no', 'n'],
+  no  : ['no' , 'n'],
 };
 
 const WINNING_COMBOS = {
@@ -13,81 +27,227 @@ const WINNING_COMBOS = {
   spock   : ['rock'    , 'scissors'],
 };
 
+const WINNING_MESSAGES = {
+  'Scissors cuts Paper'         : ['scissors', 'paper'   ],
+  'Paper covers Rock'           : ['paper'   , 'rock'    ],
+  'Rock crushes Lizard'         : ['rock'    , 'lizard'  ],
+  'Lizard poisons Spock'        : ['lizard'  , 'spock'   ],
+  'Spock smashes Scissors'      : ['spock'   , 'scissors'],
+  'Scissors decapitates Lizard' : ['scissors', 'lizard'  ],
+  'Lizard eats Paper'           : ['lizard'  , 'paper'   ],
+  'Paper disproves Spock'       : ['paper'   , 'spock'   ],
+  'Spock vaporizes Rock'        : ['spock'   , 'rock'    ],
+  'Rock crushes Scissors'       : ['rock'    , 'scissors'],
+};
+
+const ROUND_SCORE = {
+  user : 0,
+  computer : 0,
+};
+
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
-function userWins(choice, computerChoice) {
-  return WINNING_COMBOS[choice].includes(computerChoice);
+
+function userWinsRound(userChoice, computerChoice) {
+  return WINNING_COMBOS[userChoice].includes(computerChoice);
 }
 
-function displayWinner(choice, computerChoice) {
-  if (userWins(choice, computerChoice)) {
-    prompt('Congrats you win!');
-  } else if (choice === computerChoice) {
-    prompt("It's a tie!");
-  } else {
-    prompt("Sorry, computer wins!");
-  }
+
+function isValidGameChoices(input) {
+  let validChoices = Object.values(VALID_GAME_CHOICE_INPUT).flat();
+  if (validChoices.includes(input)) return input;
+  return null;
 }
 
-function getUserInput() {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let input = readline.question().toLowerCase();
-
-  while (!VALID_CHOICES.includes(input)) {
-    prompt("That's not a valid entry. Please choose from the following -");
-    prompt("(r)ock, (p)aper, (sc)issors, (sp)ock, or (l)izard");
-    input = readline.question().toLowerCase();
+function matchInputWithChoice(input) {
+  for (let [key, value] of Object.entries(VALID_GAME_CHOICE_INPUT)) {
+    if (value.includes(input)) return key;
   }
-  return input;
+  return null;
+}
+
+function getUserChoice() {
+  prompt(MESSAGES["gameChoices"]);
+  let input = readline.question().trim().toLowerCase();
+
+  while (!isValidGameChoices(input)) {
+    prompt(MESSAGES["invalidGameChoice"]);
+    prompt(MESSAGES["invalidGameChoiceOptions"]);
+    input = readline.question().trim().toLowerCase();
+  }
+  return matchInputWithChoice(input);
 }
 
 function getComputerChoice() {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  return computerChoice;
+  return VALID_CHOICES[randomIndex];
 }
 
-function displayChoices(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+function updateScore(userChoice, computerChoice) {
+  if (determineWinner(userChoice, computerChoice) === 'user') {
+    ROUND_SCORE.user += 1;
+  } else if (determineWinner(userChoice, computerChoice) === 'computer') {
+    ROUND_SCORE.computer += 1;
+  }
 }
 
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function displayPlayersMoves(userChoice, computerChoice) {
+  prompt(`You chose ${capitalize(userChoice)}, the computer chose ${capitalize(computerChoice)}`);
+}
+
+function displayWinningLogic(userChoice, computerChoice) {
+  for (let key in WINNING_MESSAGES) {
+    if (userChoice === computerChoice) {
+      return;
+    } else if (WINNING_MESSAGES[key].includes(userChoice) &&
+               WINNING_MESSAGES[key].includes(computerChoice)) {
+      prompt(key);
+    }
+  }
+}
+
+function displayRoundWinner(userChoice, computerChoice) {
+  if (userWinsRound(userChoice, computerChoice)) {
+    prompt(MESSAGES["userWinsRound"]);
+  } else if (userChoice === computerChoice) {
+    prompt(MESSAGES["tieRound"]);
+  } else {
+    prompt(MESSAGES["computerWinsRound"]);
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userWinsRound(userChoice, computerChoice)) {
+    return 'user';
+  } else if (userChoice === computerChoice) {
+    return 'tie';
+  } else {
+    return 'computer';
+  }
+}
+
+function showEndOfRoundMessages(userChoice, computerChoice) {
+  prompt(MESSAGES["dashes"]);
+  displayPlayersMoves(userChoice, computerChoice);
+  displayWinningLogic(userChoice, computerChoice);
+  displayRoundWinner(userChoice, computerChoice);
+  prompt(MESSAGES["dashes"]);
+}
+
+function continuePlaying(round, computerScore, userScore) {
+  if (computerScore === 3 ||
+      userScore     === 3 ||
+      round         === 5) {
+    prompt(MESSAGES["continueToFinalScore"]);
+  } else {
+    prompt(MESSAGES["continueToNextRound"] + (round + 1));
+  }
+
+  let continueToNextRound = readline.question();
+  return continueToNextRound;
+}
+
+function displayGameWinner(computerScore, userScore) {
+  if (computerScore > userScore) {
+    prompt(MESSAGES["dashes"]);
+    prompt(`The computer scored: ${computerScore} while you scored: ${userScore}`);
+    prompt(MESSAGES["gameLoss"]);
+    prompt(MESSAGES["dashes"]);
+  } else if (computerScore < userScore) {
+    prompt(MESSAGES["dashes"]);
+    prompt(`You scored: ${userScore} while the computer scored: ${computerScore}`);
+    prompt(MESSAGES["gameWon"]);
+    prompt(MESSAGES["dashes"]);
+  } else {
+    prompt(MESSAGES["dashes"]);
+    prompt(MESSAGES["gameTie"]);
+    prompt(MESSAGES["dashes"]);
+  }
+}
+
+function displayScoreBoard(userScore, computerScore, round) {
+  prompt(MESSAGES["dashes"]);
+  prompt(`Round ${round}. User: ${userScore} - Computer: ${computerScore}`);
+  prompt(MESSAGES["dashes"]);
+}
 
 function playGame() {
-  let choice = getUserInput();
-  let computerChoice = getComputerChoice();
+  for (let round = 1; round <= 5; round += 1) {
+    displayScoreBoard(ROUND_SCORE.user, ROUND_SCORE.computer, round);
 
-  displayChoices(choice, computerChoice);
-  displayWinner(choice, computerChoice);
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+
+    updateScore(userChoice, computerChoice);
+
+    console.clear();
+
+    showEndOfRoundMessages(userChoice, computerChoice);
+
+    continuePlaying(round, ROUND_SCORE.computer, ROUND_SCORE.user);
+    console.clear();
+
+    if ((ROUND_SCORE.computer === 3) || (ROUND_SCORE.user === 3)) break;
+  }
+  displayGameWinner(ROUND_SCORE.computer, ROUND_SCORE.user);
 }
 
-function validPlayAgain(input) {
+function isValidPlayAgain(input) {
   let answers = Object.values(VALID_PLAY_AGAIN_CHOICES);
   let flatAnswers = answers.flat();
   if (flatAnswers.includes(input)) return true;
+  return null;
 }
 
-function playAgain() {
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question().toLowerCase();
+function endGameChoices(input) {
+  for (let [key, value] of Object.entries(VALID_PLAY_AGAIN_CHOICES)) {
+    if (value.includes(input)) return key;
+  }
+  return null;
+}
 
-  while (!validPlayAgain(answer)) {
-    prompt("That's not a valid choice");
-    prompt("Please enter (y)es or (n)o");
-    answer = readline.question().toLowerCase();
+function endGame() {
+  prompt(MESSAGES["playAgain"]);
+  let answer = readline.question().trim().toLowerCase();
+
+  while (!isValidPlayAgain(answer)) {
+    prompt(MESSAGES["invalidPlayAgain"]);
+    answer = readline.question().trim().toLowerCase();
   }
 
-  if ((answer === 'no') || (answer === 'n')) {
+  if (endGameChoices(answer) === 'no') {
     return answer;
   } else {
     return null;
   }
 }
 
-do {
-  playGame();
-  if (playAgain()) break;
+function showWelcomeMessages() {
+  prompt(MESSAGES['welcome']);
+  prompt(MESSAGES["rules"]);
+  prompt(MESSAGES["dashes"]);
+  prompt(MESSAGES["overView"]);
+  prompt(MESSAGES["dashes"]);
+  prompt(MESSAGES["continueToGame"]);
+  let continueToGame = readline.question();
+  return continueToGame;
+}
 
-} while (true);
+function main() {
+  showWelcomeMessages();
 
+  do {
+    console.clear();
+    playGame();
+    if (endGame()) break;
+  }
+  while (true);
+}
+
+main();
